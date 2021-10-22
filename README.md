4026601fe1480e6f85510eefaae8a2d83f075e3a
AZMIAO <54100327+azmiao@users.noreply.github.com>
2021-10-22 17:41:04 +0800

    updated!

---
+++ README.md
@@ -0,0 +1,35 @@
+## pcr_almanac
+
+一个适用hoshinobot的 pcr签到黄历 插件
+
+功能仿造自[BillYang2016](https://github.com/BillYang2016)的酷Q黄历插件
+
+甚至`data.json`的配置文件直接拿来用了，已获原作者授权
+
+插件后续将继续在 github 不定期更新，欢迎提交 isuue 和 request
+
+本插件仅供学习研究使用，插件免费，请勿用于违法商业用途，一切后果自己承担
+
+## 项目地址：
+
+https://github.com/azmiao/pcr_almanac
+
+## 功能
+
+```
+目前就一个命令：
+[签到] 签到看黄历
+```
+
+## 简单食用教程：
+
+1. 下载或git clone本插件：
+
+    在 HoshinoBot\hoshino\modules 目录下使用以下命令拉取本项目
+    ```
+    git clone https://github.com/azmiao/pcr_almanac
+    ```
+
+2. 在 HoshinoBot\hoshino\config\ `__bot__.py` 文件的 MODULES_ON 加入 'pcr_almanac'
+
+    然后重启 HoshinoBot
---
+++ get_all_info.py
@@ -0,0 +1,44 @@
+import os
+import json
+import random
+import datetime
+from .update_data import *
+
+def get_json():
+    current_dir = os.path.join(os.path.dirname(__file__), 'data.json')
+    file = open(current_dir, 'r', encoding = 'utf-8')
+    config = json.load(file)
+    return config
+
+def get_time():
+    now_time = datetime.datetime.now()
+    now_time = now_time.strftime('%Y-%m-%d %H:%M:%S')
+    return now_time
+
+def get_msg(group_id, user_id):
+    config = get_json()
+    msg = f'[CQ:at,qq={user_id}]签到成功'
+    fortune = random.choice(config['fortune'])
+    now_time = str(get_time())
+    characters = random.choice(config['characters'])
+    suitable_key = random.choice(list(config['suitable'].keys()))
+    suitable_value = config['suitable'][suitable_key]
+    suitable = suitable_key + '：' + suitable_value
+    unsuitable_key = random.choice(list(config['unsuitable'].keys()))
+    unsuitable_value = config['unsuitable'][unsuitable_key]
+    unsuitable = unsuitable_key + '：' + unsuitable_value
+    prefertime = f'{int(random.random() * 24)}时' + f'{int(random.random() * 60)}分' + f'{int(random.random() * 60)}秒'
+    position = random.choice(config['position'])
+    actions = random.choice(config['actions'])
+    write_info(group_id, user_id, actions, characters, fortune, position, prefertime, suitable_key, suitable_value, unsuitable_key, unsuitable_value)
+    msg = '{}\n今日运势：{}\n当前时间：{}\n今日幸运角色：{}\n宜：{}\n忌：{}\n抽卡加成时间：{}\n抽卡加成方向：{}\n抽卡加成动作：{}'.format(msg, fortune, now_time, characters, suitable, unsuitable, prefertime, position, actions)
+    return msg
+
+def judge(group_id, user_id):
+    current_dir = os.path.join(os.path.dirname(__file__), f'data\{group_id}.json')
+    if os.path.exists(current_dir):
+        file = open(current_dir, 'r', encoding = 'UTF-8')
+        config = json.load(file)
+        if str(user_id) in list(config.keys()):
+            return True
+    return False
